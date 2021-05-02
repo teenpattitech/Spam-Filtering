cfca5cb3baaac8c13afeb4210ac036bcc9784ce1
Warren <warren.fernandes27@gmail.com>
2021-05-02 11:06:06 +0530

    Integrated model with Streamlit app

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import streamlit as st
 import time
+import pickle
+import nltk
+import re
+nltk.download('wordnet')
+nltk.download('stopwords')
+from nltk.stem import WordNetLemmatizer
+
+def predict_mail(mail):
+    
+    model = pickle.load(open("model.pckl",mode="rb"))
+    vectorizer = pickle.load(open("vectorizer.pckl",mode="rb"))
+    
+    lemma = WordNetLemmatizer()
+    
+    stopwords = nltk.corpus.stopwords.words('english')
+    
+    mail = re.sub(r"http\S+", "", mail)
+    mail = re.sub("[^a-zA-Z0-9]"," ",mail)
+    mail = mail.lower()
+    mail = nltk.word_tokenize(mail)
+    mail = [lemma.lemmatize(word) for word in mail]
+    mail = [word for word in mail if word not in stopwords]
+    mail = " ".join(mail)
+    
+    vector = vectorizer.transform([mail])
+    decision = model.predict(vector.toarray())
+    
+    return decision[0]
 
 st.title("Welcome to Spam Filtering")
 
@@ -42,10 +70,21 @@ if nav == "About":
 
 if nav == "Filter":
     st.markdown("# Hi, I am your _buddy Email Spam Filtering_")
-    st.markdown("## Click here to activate me")
-    if(st.button("Activate")):
-        progress = st.progress(0)
-        for i in range(100):
-            time.sleep(0.1)
-            progress.progress(i+1)
-        st.balloons()
+#     st.markdown("## Click here to activate me")
+#     if(st.button("Activate")):
+#         progress = st.progress(0)
+#         for i in range(100):
+#             time.sleep(0.1)
+#             progress.progress(i+1)
+#         st.balloons()
+    sentence = st.text_area("Input your sentence here:") 
+
+    if st.button("Predict"):
+        output = predict_mail(sentence)
+        if output == 1:
+            st.markdown('Its a Spam Email')
+        else:
+            st.markdown('Its Not a Spam Email')
+        
+     
+                                                               
